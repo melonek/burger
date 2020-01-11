17fff0e71de798104a8ddf4b25fff2c40b80a10c
melon <meloniusz@hotmail.com>
2020-01-11 17:49:41 +0800

    prepared the server.js file - server run o.k

---
+++ server.js
@@ -0,0 +1,20 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let exphbs = require("express-handlebars");
+
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+let routes = require("./controllers/burgers_controler.js");
+
+app.listen(PORT, function() {
+  console.log("Server listening on http://localhost:" + PORT);
+});
