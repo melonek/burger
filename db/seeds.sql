9620628bc18a715e321fd2af2bdb997246f5ee09
melon <meloniusz@hotmail.com>
2020-01-11 17:37:49 +0800

    prepared mysql files schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Bob's Burger", false), ("Beefcakes Burger", false), ("Backyard Burger", false), ("The Crispy Bun Burger", false), ("Toasty Buns Burger", false);
