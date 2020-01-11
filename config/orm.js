d570ba7afbea24dd79af9c1432e0609222e927f7
melon <meloniusz@hotmail.com>
2020-01-11 19:18:48 +0800

    translateSql

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,19 @@
 let connection = require("../config/connection");
 
+function translateSql(ob) {
+  var arr = [];
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
 let orm = {
   selectAll: function(table, cb) {
     var dbQuery = "SELECT * FROM " + table + ";";
