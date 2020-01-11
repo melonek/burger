ae54dba4a910027b2155697151bd56e7058e3a65
melon <meloniusz@hotmail.com>
2020-01-11 19:34:37 +0800

    commas added to burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,25 +1,27 @@
-let orm = require("../config/orm");
-
-let burger = {
+var orm = require("../config/orm.js");
+var burger = {
   selectAll: function(cb) {
     orm.selectAll("burgers", function(res) {
-      cb.json(res);
+      cb(res);
     });
-  }
+  },
+
   insertOne: function(cols, vals, cb) {
-      orm.selectAll("burgers", function(res){
-          cb.json(res);
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
     });
-  }
+  },
+
   updateOne: function(objColVals, condition, cb) {
-      orm.selectAll("burgers", objColVals, condition, function(res){
-          cb.json(res)
-      })
-  }
-  deleteOne: function(condition, cb){
-      orm.selectAll("burgers", condition, function(res){
-        cb.json(res)
-      })
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+
+  deleteOne: function(condition, cb) {
+    orm.deleteOne("burgers", condition, function(res) {
+      cb(res);
+    });
   }
 };
 
