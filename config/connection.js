bfdf51232253ef650adcca7355f975cb6813cc8e
melon <meloniusz@hotmail.com>
2020-01-11 20:11:29 +0800

    change to sql

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
-let mysql = require("mysql");
-connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "helloworld",
-  database: "burgers_db"
-});
-connection.connect(function(err) {
-  if (err) {
-    console.log("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "helloworld",
+    database: "burgers_db"
+  });
+}
+connection.connect();
 module.exports = connection;
