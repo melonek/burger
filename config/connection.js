f897383e9fa61d9b3807cb4f67ba0c6d78d9123b
melon <meloniusz@hotmail.com>
2020-01-11 17:57:50 +0800

    connection.js o.k + mysql database established

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+let mysql = require("mysql");
+connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "helloworld",
+  database: "burgers_db"
+});
+connection.connect(function(err) {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+module.exports = connection;
