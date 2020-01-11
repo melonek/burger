cf73733bd56376d5e774ea7ee4c572688ac63faa
melon <meloniusz@hotmail.com>
2020-01-11 20:26:46 +0800

    corretion to name of the file

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-let routes = require("./controllers/burgers_controler.js");
+let routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
 app.listen(PORT, function() {
-  console.log("Server listening on http://localhost:" + PORT);
+  console.log("Server listening on: http://localhost:" + PORT);
 });
