2bbe20c081d66a983e61e3fc74080b6941401306
melon <meloniusz@hotmail.com>
2020-01-11 20:14:12 +0800

    changes

--- controllers/burgers_controler.js
+++ controllers/burgers_controler.js
@@ -5,11 +5,11 @@ let router = express.Router();
 
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
-    let handlebarsObj = {
+    let hdbrsObj = {
       burgers: data
     };
-    console.log(handlebarsObj);
-    res.render("index", handlebarsObj);
+    console.log(hdbrsObj);
+    res.render("index", hdbrsObj);
   });
 
   router.post("/api/burgers", function(req, res) {
@@ -17,6 +17,7 @@ router.get("/", function(req, res) {
       ["burger_name", "devoured"],
       [req.body.burger_name, req.body.devoured],
       function(result) {
+        // Send back the ID of new burger
         res.json({ id: result.insertId });
       }
     );
@@ -28,19 +29,19 @@ router.get("/", function(req, res) {
     burger.updateOne({ devoured: req.body.devoured }, condition, function(
       result
     ) {
-      if ((result, changeRows === 0)) {
+      if (result.changedRows === 0) {
         return res.status(404).end();
       } else {
         res.status(200).end();
       }
     });
   });
-  router.deleteOne(condition, function(req, res) {
+  router.delete("/api/burgers/:id", function(req, res) {
     let condition = "id = " + req.params.id;
-    console.group("condition", condition);
+    console.log("condition", condition);
 
     burger.deleteOne(condition, function(result) {
-      if ((result, changeRows === 0)) {
+      if (result.changedRows === 0) {
         return res.status(404).end();
       } else {
         res.status(200).end();
